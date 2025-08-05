afc06cbc59741ed110b02a4ca6cdaaff69877371
Kenichiro Nishioka <nishioka.kenichiro@gmail.com>
2025-08-05 10:29:54 +0800

    fix: setup-local.shの文末に改行を追加

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   '*.{js,jsx,ts,tsx}': ['eslint --fix', 'prettier --write'],
-  '*.{json,md,yml,yaml}': ['prettier --write'],
+  '*.{json,md,yml,yaml,sh}': ['prettier --write'],
   'package.json': () => 'pnpm install --frozen-lockfile',
   '**/*.{ts,tsx}': () => [
     // 変更されたファイルが含まれるパッケージの型チェックを実行
